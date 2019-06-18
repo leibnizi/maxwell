0aa860828881b58a6f6bc4bc30487b7077a9f282
leibniz <learnwen@gmail.com>
2019-06-18 16:09:32 +0800

    add mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import { observer, inject } from "mobx-react";
+
+import { IMobxStore } from "./stores/mobxStore";
 import "./App.css";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
+interface AppProps {
+  mobxStore?: IMobxStore;
+}
+
+@inject(function(stores: any) {
+  return {
+    name: stores.mobxStore.name,
+    greeting: stores.mobxStore.greeting,
+    setName: stores.mobxStore.setName
+  };
+})
+@observer
+class App extends Component<any> {
+  clickHandler = () => {
+    const { setName } = this.props;
+    setName("Bobs");
+  };
+  render() {
+    const { greeting, name } = this.props;
+    console.log("name: ", name);
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {name}
+          <button onClick={this.clickHandler}>Change Greeting</button>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { Provider } from "mobx-react";
+
+import { stores } from "./stores";
 import App from "./App";
+import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider {...stores}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-@testable
-class MyTestableClass {
-  // ...
-}
-
-function testable(target: any) {
-  target.isTestable = true;
-}
-console.log("MyTestableClass", MyTestableClass);
